b3d1ac2e3287191d3e07d4ed837ee75ad48073d2
Dericeda <nurasyl.asanov@mail.ru>
2025-11-13 10:45:41 +0500

    added incapsulation for the junior and senior class attributes

--- src/JuniorDeveloper.java
+++ src/JuniorDeveloper.java
@@ -1,15 +1,33 @@
 public class JuniorDeveloper implements IDeveloper {
+    private String classMessage = "Writing poor class...";
+    private String databaseMessage = "Drop database...";
+    private String testMessage = "Creating not reliable test...";
+
 
     @Override
     public void create(ProjectClass projectClass){
-        System.out.println("Writing poor class...");
+        System.out.println(classMessage);
     }
     @Override
     public void create(Database database){
-        System.out.println("Drop database...");
+        System.out.println(databaseMessage);
     }
     @Override
     public void create(Test test){
-        System.out.println("Creating not reliable test...");
+        System.out.println(testMessage);
     }
+
+    public void setClassMessage(String message){
+        this.classMessage = message;
+    }
+
+    public void setDatabaseMessage(String message){
+        this.databaseMessage = message;
+    }
+    public void setTestMessage(String message){
+        this.testMessage = message;
+    }
+
+
+
 }
--- src/SeniorDeveloper.java
+++ src/SeniorDeveloper.java
@@ -1,14 +1,33 @@
-public class SeniorDeveloper implements  IDeveloper{
+public class SeniorDeveloper implements IDeveloper {
+    private String classMessage = "Rewriting class after junior...";
+    private String databaseMessage = "Fixing database...";
+    private String testMessage = "Creating reliable test...";
+
+
     @Override
     public void create(ProjectClass projectClass){
-        System.out.println("Rewriting class after junior...");
+        System.out.println(classMessage);
     }
+
     @Override
     public void create(Database database){
-        System.out.println("Fixing database...");
+        System.out.println(databaseMessage);
     }
+
     @Override
     public void create(Test test){
-        System.out.println("Creating reliable test...");
+        System.out.println(testMessage);
+    }
+
+    public void setClassMessage(String message){
+        this.classMessage = message;
+    }
+
+    public void setDatabaseMessage(String message){
+        this.databaseMessage = message;
+    }
+
+    public void setTestMessage(String message){
+        this.testMessage = message;
     }
 }
